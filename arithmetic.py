13a24a3659ddaff47fa9982a6d17d5a0f4a90cd7
Max F. Gjacqual <89233376+gjacqual@users.noreply.github.com>
2021-12-25 21:03:13 +0300

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,4 +1,4 @@
-def arithmetic(operation):
+def arithmetic(expression):
     def separator(symb, str):
         find = str.find(symb)
         if find != -1:
@@ -7,48 +7,50 @@ def arithmetic(operation):
         else:
             return False
 
-    argums = separator("+", operation)
-    if argums:
-        return int(argums[0]) + int(argums[1])
-    else:
-        argums = separator("-", operation)
-        if argums:
-            return int(argums[0]) - int(argums[1])
-        else:
-            argums = separator("%", operation)
-            if argums:
-                return int(argums[0]) / 100
-            else:
-                argums = separator("/", operation)
+    operations = ['+', '-', '%', '/', '*']
+
+    for op in operations:
+        if expression.find(op):
+            argums = separator(op, expression)
+            if op == '+':
+                if argums:
+                    return int(argums[0]) + int(argums[1])
+            elif op == '-':
+                if argums:
+                    return int(argums[0]) - int(argums[1])
+            elif op == '%':
+                if argums:
+                    return int(argums[0]) / 100
+            elif op == '/':
                 if argums:
                     return int(argums[0]) / int(argums[1])
-                else:
-                    stars = 0
-                    for c in operation:
-                        if c == "*":
-                            stars += 1
-                    if stars == 1:
-                        argums = separator("*", operation)
-                        if argums:
-                            return int(argums[0]) * int(argums[1])
+            elif op == '*':
+                stars = 0
+                for c in expression:
+                    if c == "*":
+                        stars += 1
+                if stars == 1:
+                    argums = separator("*", expression)
+                    if argums:
+                        return int(argums[0]) * int(argums[1])
+                    else:
+                        return "None"
+                elif stars == 2:
+                    argums = separator("**", expression)
+                    if argums:
+                        if argums[1] == '':
+                            return int(argums[0]) * int(argums[0])
                         else:
-                            return "None"
-                    elif stars == 2:
-                        argums = separator("**", operation)
-                        if argums:
-                            if argums[1] == '':
-                                return int(argums[0]) * int(argums[0])
-                            else:
-                                res = int(argums[0])
-                                i = 1
-                                while i < int(argums[1]):
-                                    res *= int(argums[0])
-                                    i += 1
-                                return res
-                        else:
-                            return "None"
-expresssion = input("Enter arithmetic expression: ")
-print(arithmetic(expresssion))
+                            res = int(argums[0])
+                            i = 1
+                            while i < int(argums[1]):
+                                res *= int(argums[0])
+                                i += 1
+                            return res
+                    else:
+                        return "None"
+expression = input("Enter arithmetic expression: ")
+print(arithmetic(expression))
 
 
 
